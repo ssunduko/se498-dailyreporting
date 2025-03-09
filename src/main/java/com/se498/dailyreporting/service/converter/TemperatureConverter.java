09f37f51904d12ee64717d65fe817ce7a7e71b03
Sergey Sundukovskiy <ssunduko@gmail.com>
2025-03-09 01:20:09 -0800

    added Patterns

--- src/main/java/com/se498/dailyreporting/service/converter/TemperatureConverter.java
+++ src/main/java/com/se498/dailyreporting/service/converter/TemperatureConverter.java
@@ -1,16 +1,35 @@
 package com.se498.dailyreporting.service.converter;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.Scanner;
 
 public class TemperatureConverter {
 
-    private static final Map<Double,Double> db = new HashMap(){{
-        put(0.0,32.0);
-        put(1.0,33.8);
-        put(2.0,35.6);
-    }};
-    double convert (double numberToConvert) {
+    static Double convert(double numberToConvert) {
         return numberToConvert * 9/5 + 32;
+    }
+    static Double convert(double numberToConvert, String conversionScale){
+        if (conversionScale.equalsIgnoreCase("Celsius to Fahrenheit")){
+            return convert(numberToConvert);
+        } else {
+            System.out.println("Invalid Scale");
+        }
+        return null;
+    }
+    private static double readNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter Number To Convert: ");
+        return scanner.nextDouble();
+    }
+    private static String readConversionScale(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter Conversion Scale: ");
+        return scanner.nextLine();
+
+    }
+    public static void main(String[] args) throws Exception {
+        double numberToConvert = readNumber();
+        String conversionScale = readConversionScale();
+        System.out.println("Converted Number: " + convert(numberToConvert, conversionScale));
     }
 }
