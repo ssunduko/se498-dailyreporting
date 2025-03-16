6b5eaa8025df5c799028b71b9cbfb9fa0f9488dc
Sergey Sundukovskiy <ssunduko@gmail.com>
2025-03-16 03:40:39 -0700

    selenium functional test

--- src/test/java/com/se498/dailyreporting/ui/DailyReportTest.java
+++ src/test/java/com/se498/dailyreporting/ui/DailyReportTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Selenium tests for Daily Report functionality
  */
+@ActiveProfiles("test")
 public class DailyReportTest extends BaseSeleniumTest {
 
     private String createTestReport() {
